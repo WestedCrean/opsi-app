464e62c3294c4f074d83fd26d63669bc8874b9fc
Wiktor Flis <flis.wf@pg.com>
2021-05-20 06:20:55 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import streamlit as st
+from src.algorytmy.crc import Crc
+
+#st.header('Aplikacja OPSI')
+
+algo = Crc()
+
+algo.output()
---
+++ src/algorytmy/crc.py
@@ -0,0 +1,10 @@
+from src.algorytmy.abstractAlgorithm import AbstractAlgorithm
+
+
+class Crc(AbstractAlgorithm):
+
+    def __init__(self):
+        pass
+
+    def output(self):
+        print("test")
